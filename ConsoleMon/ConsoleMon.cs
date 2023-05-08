8b11e96c159f8bd215c9c96cdf82500284cffe1a
Gino <35615@ma-web.nl>
2023-05-08 12:58:35 +0200

    functions 1 af met de class opdrachten

---
+++ ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+
+    internal class ConsoleMon
+    {
+        public int health;
+        public int energy;
+        string name;
+
+
+        public void TakeDamage(int damage)
+        {
+            this.health -= damage;
+        }
+
+        public void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+        
+    }
+
+}
