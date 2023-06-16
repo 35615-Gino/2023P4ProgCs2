1cc7f5df95e9b10e508f325284544d49448b297c
Gino <35615@ma-web.nl>
2023-06-16 09:41:26 +0200

    Kaas

--- ja/Program.cs
+++ ja/Program.cs
@@ -6,7 +6,30 @@ namespace ja
     {
         static void Main(string[] args)
         {
+            string text = "Lisa heeft psychologische hulp nodig waarbij ze het uit moet maken met Peter om soep te gaan eten waarbij ze water groen gaat eten geel zwart maakt en gras kaas is";
 
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Je moeder is cringe");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ReadLine();
+            Console.ResetColor();
+
+            foreach (char words in text)
+            {
+                Console.Write(words);
+                Thread.Sleep(100);
+            }
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.ReadLine();
+            Console.ResetColor();
+        }
+
+
+        internal void Soup ()
+        {
             int[,,] mapData = new int[0, 0, 1];
 
             for (int i = 0; i < 0; i++)         //map tile type
@@ -47,46 +70,6 @@ namespace ja
                 }
             } //map display lmaos
             Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            string text = "Lisa heeft psychologische hulp nodig waarbij ze het uit moet maken met Peter om soep te gaan eten waarbij ze water groen gaat eten geel zwart maakt en gras kaas is";
-
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.WriteLine("Lisa get help");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.ReadLine();
-            Console.ResetColor();
-
-            foreach (char words in text)
-            {
-                Console.Write(words);
-                Thread.Sleep(100);
-            }
-            Console.ReadKey();
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.ReadLine();
-            Console.ResetColor();
-        }
-
-
-        internal void Soup ()
-        {
         }
     }
 }
